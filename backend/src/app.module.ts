b2bf9aaf803eae424853d3bcf9b3d15df30c685a
MohamedHalat <halatmohamed@hotmail.com>
2022-03-05 23:34:19 -0500

    chore: remove jest and add graphql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
