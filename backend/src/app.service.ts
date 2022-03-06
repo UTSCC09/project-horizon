21bd9139e63b24fef06a2db9e74ae095cb070edc
MohamedHalat <halatmohamed@hotmail.com>
2022-03-05 23:34:19 -0500

    chore: remove jest and add graphql

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Query } from '@nestjs/graphql';
 
 @Injectable()
 export class AppService {
+
+  @Query(() => String)
   getHello(): string {
     return 'Hello World!';
   }
