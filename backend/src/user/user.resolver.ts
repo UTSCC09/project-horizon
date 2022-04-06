7c92ca2e7f478bf946f154f23b4ae8e753b114b7
Ahmed Halat <ahmedhalat@gmail.com>
2022-04-06 18:54:01 -0400

    chore: create pattern subscriber

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -10,13 +10,18 @@ import { UserService } from './user.service';
 @UseGuards(GqlAuthGuard)
 export class UserResolver {
 	private subscriber;
-	private activeChannels: string[] = [];
+	private subscribed: boolean = false;
 
 	constructor(
 		private readonly userService: UserService,
 		private readonly redis: RedisService,
 	) {
 		this.subscriber = this.redis.subscriber;
+
+		this.subscriber.psubscribe('notifications*', (err: any, count: number) => {
+			if (err) return console.error(err);
+			this.subscribed = true;
+		});
 	}
 
 	@Query(() => User)
@@ -34,33 +39,14 @@ export class UserResolver {
 	@Query(() => Notification)
 	async notifications(@RequestUser() user: User) {
 		const channel = `notifications:${user.id}`;
-		this.activeChannels.push(channel);
-
-		this.subscriber.subscribe(...this.activeChannels, (err, count) => {
-			if (err) {
-				//  return 500
-				throw new HttpException(
-					'Failed to subscribe to notifications',
-					HttpStatus.INTERNAL_SERVER_ERROR,
-				);
-			}
-
-			if (count == 0) {
-				throw new HttpException(
-					'Failed to subscribe to notifications channel',
-					HttpStatus.INTERNAL_SERVER_ERROR,
-				);
-			}
-		});
+		if (!this.subscriber) {
+			throw new HttpException('Failed to connect to notification channel', HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 
 		return await new Promise((resolve, reject) => {
-			this.subscriber.on('message', (msgChannel, message) => {
+			this.subscriber.on('pmessage', (_pattern, msgChannel, message) => {
 				if (msgChannel != channel) return;
-
 				const notification = JSON.parse(message);
-
-				this.subscriber.unsubscribe(channel);
-				this.activeChannels = this.activeChannels.filter(c => c !== channel);
 
 				return resolve(notification);
 			});
